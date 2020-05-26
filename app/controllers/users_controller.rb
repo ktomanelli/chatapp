67ba0dce33d54cc450f7705aa67850ada1b41619
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-05-26 17:00:56 -0400

    user crud and views setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @users = User.all
+    end
+    
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.create(user_params)
+
+        if @user.valid?
+            session[:user_id] = @user.id
+            redirect_to user_path(@user.id)
+        else
+            flash[:errors] = @user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        @user.update(user_params)
+        redirect_to user_path(@user.id)
+    end
+    
+    def show
+    
+    end
+    
+    def destroy
+        @user.destroy
+        redirect_to new_user_path
+    end
+
+    private
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :username, :img_url, :bio, :password, :password_confirmation)
+    end
 end
