e18b934508eb99a261a5449066a55f9cd9499bfe
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-05-26 18:02:42 -0400

    added message collection for chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,9 @@
 class ChatroomsController < ApplicationController
     before_action :find_chatroom, only: [:show, :edit, :update, :destroy]
     
+    def index
+        
+    end
     def new
         @chatroom = Chatroom.new
     end
@@ -41,4 +44,13 @@ class ChatroomsController < ApplicationController
         params.require(:chatroom).permit(:title)
     end
 
+
+    def display_messages
+        if params[:id]
+            @chatroom = Chatroom.find(params[:id])
+            @messages = @chatroom.messages.sort do |a,b|
+                a.created_at<=>b.created_at
+            end
+        end
+    end
 end
