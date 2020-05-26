bd63d3719bc8476011e6ab652cc3aec83be2211f
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-05-26 16:34:46 -0400

    CRUD functions

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,2 +1,44 @@
 class ChatroomsController < ApplicationController
+    before_action :find_chatroom, only: [:show, :edit, :update, :destroy]
+    
+    def new
+        @chatroom = Chatroom.new
+    end
+    
+    def create
+        @chatroom = Chatroom.create(chatroom_params)
+        if @chatroom.valid?
+            redirect_to chatroom_path(@chatroom.id)
+        else 
+           flash[:errors] = @chatroom.errors.full_messages
+           redirect_to new_chatroom_path 
+        end
+    end
+    
+    def edit
+    
+    end
+    
+    def update
+        @chatroom.update(chatroom_params)
+        redirect_to chatrooms_path
+    end
+    
+    def show
+    end
+    
+    def destroy
+        @chatroom.destroy
+        redirect_to chatrooms_path
+    end
+
+    private
+    def find_chatroom
+        @chatroom = Chatroom.find(params[:id])
+    end
+
+    def chatroom_params
+        params.require(:chatroom).permit(:title)
+    end
+
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,40 @@
 class MessagesController < ApplicationController
+
+    
+    def new
+        @message = Message.new
+    end
+    
+    def create
+        @message = Message.create(message_params)
+        if @message.valid?
+            redirect_to message_path(@message)
+        else
+            flash[:errors] = @message.errors.full_messages
+            redirect_to new_message_path
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        @message.update(message_params)
+        redirect_to message_path(@message.id)
+    end
+    
+    
+    def destroy
+        @message.destroy
+        redirect_to new_message_path
+    end
+
+    private 
+    def find_message 
+        @message = Message.find(params[:id])
+    end
+
+    def message_params
+        params.require(:message).permit(:content, :from_id, :to_id, :chatroom_id)
+    end
 end
