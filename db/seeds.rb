191d83791c4f1ac4fb40582207f12fa8b56f60db
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-05-26 20:08:41 -0400

    added more data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -14,17 +15,27 @@ Reaction.destroy_all
 
 ramon = User.create(name:'Ramon',username:'ramon',password:'r12345')
 kyle = User.create(name:'Kyle',username:'kyle',password:'k12345')
-john = User.create(name:'John',username:'john',password:'j12345')
 
-cr1=Chatroom.create(title:'Ramon and Kyle')
-cr2=Chatroom.create(title:'Kyle and John')
+15.times do 
+    User.create(name:Faker::Name.name,username:"#{Faker::Name.initials(number: 1)+Faker::Name.last_name}",password:"#{Faker::Name.initials(number: 1)}12345")
+end
 
-m1 = Message.create(content:'Hey!',from_id:kyle.id,to_id:ramon.id,chatroom_id:cr1.id)
-m2 = Message.create(content:'Hello',from_id:ramon.id,to_id:kyle.id,chatroom_id:cr1.id)
-m3 = Message.create(content:"what's up?",from_id:kyle.id,to_id:ramon.id,chatroom_id:cr1.id)
-m4 = Message.create(content:'nm u?',from_id:ramon.id,to_id:kyle.id,chatroom_id:cr1.id)
-m5 = Message.create(content:'Hey John',from_id:kyle.id,to_id:john.id,chatroom_id:cr2.id)
-m6 = Message.create(content:'Hey Kyle',from_id:john.id,to_id:kyle.id,chatroom_id:cr2.id)
-m7 = Message.create(content:'whats up?',from_id:kyle.id,to_id:john.id,chatroom_id:cr2.id)
-m8 = Message.create(content:'nm jc u?',from_id:john.id,to_id:kyle.id,chatroom_id:cr2.id)
-m9 = Message.create(content:'same',from_id:kyle.id,to_id:john.id,chatroom_id:cr2.id)
+def get_other(array,val)
+    if val==array[0]
+        return array[1]
+    end
+    return array[0]
+end
+
+25.times do 
+    users=[User.all.sample,User.all.sample]
+    if users[0] != users[1]
+        c=Chatroom.create(title:"#{users[0].name} + #{users[1].name}")
+        u1=users.sample
+        u2=get_other(users,u1)
+        rand(20..50).times do
+            Message.create(content:Faker::Lorem.sentences(number: 2),from_id:u1.id,to_id:u2.id,chatroom_id:c.id)
+        end
+    end
+end
+
